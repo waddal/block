c515667b2ef1d33ff0814537ef80ba705cb1b0ab
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-10-29 15:11:04 -0700

    init

---
+++ src/Experience/Experience.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
+import Camera from "./Camera";
+import Renderer from "./Renderer";
+
+export default class Experience {
+  static instance;
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+    Experience.instance = this;
+    this.canvas = canvas;
+    this.scene = new THREE.Scene();
+    this.time = new Time();
+    this.sizes = new Sizes();
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+
+    this.time.on("update", () => {
+      this.update();
+    });
+  }
+
+  update() {
+    this.camera.update();
+    this.renderer.update();
+  }
+}
---
+++ src/Experience/Renderer.js
@@ -0,0 +1,41 @@
+import Experience from "./Experience";
+import * as THREE from "three";
+
+export default class Renderer {
+  constructor() {
+    this.experience = new Experience(this.canvas);
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+    this.camera = this.experience.camera;
+
+    console.log(this.camera);
+
+    this.setRenderer();
+  }
+
+  setRenderer() {
+    this.renderer = new THREE.WebGL1Renderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.toneMapping = THREE.CineonToneMapping;
+    this.renderer.toneMappingExposure = 1.75;
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFShadowMap;
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
+    this.renderer.setPixelRatio(this.sizes.pixelRatio);
+  }
+
+  resize() {
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
+    this.renderer.setPixelRatio(this.sizes.pixelRatio);
+  }
+
+  update() {
+    this.renderer.render(this.scene, this.camera.perspectiveCamera);
+  }
+}
